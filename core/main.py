1c0305f1d42211e679de27c63c0655d5bef49bd0
Kauan Sotério de Azevedo <kauan.azevedo@jeaholding.com.br>
2023-05-15 15:26:50 -0300

    feat: escolhas

--- core/main.py
+++ core/main.py
@@ -15,7 +15,31 @@ def main() -> None:
 
     print("Bem-vindo")
     while not sair:
-        ...
+        print("\n0 - Sair\n1 - Registrar Hospital\n2 - Listar Hospitais\n3 - Atualizar Hospital")
+        try:
+            escolha = int(input("Escolha: "))
+        except:
+            print("\nSomente numeros sao validos!")
+
+        if escolha == 0:
+            print("\nAté mais!\n\n")
+            break
+        elif escolha == 1:
+            try:
+                nome = str(input("(Hospital)Digite o nome: "))
+                endereco = str(input("(Hospital)Digite o endereco: "))
+                cep = str(input("(Hospital)Digite o CEP: "))
+            except:
+                print("Valores invalidos!")
+                pass
+            finally:
+                if not nome == "" or not endereco == "" or not cep == "":
+                    print("Dados salvos!")
+                else:
+                    print("Nome, Endereco e/ou Cep invalido(s)")
+
+        else:
+            print("Valor Invalido!\n")
 
 
 if __name__ == "__main__":
