5aca18031f71f1e29600bdb00ac84bbe7ae5dd45
Kauan-Azevedo <kauanazevedo2010@hotmail.com>
2023-05-16 14:20:19 -0300

    fix: hospital-py

---
+++ core/structured/hospital.py
@@ -0,0 +1,67 @@
+def create_hospital(db_conn, name: str, address: str, cep: str):
+    cursor = db_conn.cursor()
+    
+    try:
+        query: str = "INSERT INTO Hospital(nome, endereco, cep) VALUES(%(name)s, %(address)s, %(cep)s)"
+        query_params: dict = {"name": name, "address": address, "cep": cep}
+        cursor.execute(query, query_params)
+        db_conn.commit()
+    except:
+        print("Falha ao inserir os dados")
+    finally:
+        print("Dados salvos com sucesso!")
+
+    cursor.close()
+    
+def read_all_hospital(db_conn):
+    cursor = db_conn.cursor()
+    
+    try:
+        query: str = "SELECT * FROM Hospital"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        
+        print("\n+---------- Hospitais ----------+\n")
+        for row in result:
+            print(f"Id: {row[0]}\nNome: {row[1]}\nEndereco: {row[2]}\nCep: {row[3]}")
+
+            print("+--------------------+")
+        print("\n+---------- Fim ----------+")
+        
+    except:
+        print("Falha ao completar a tarefa")
+
+
+    cursor.close()
+
+def update_hospital(db_conn, hospital_name: str, name: str, address: str, cep: str):
+    cursor = db_conn.cursor()
+
+    try:
+        query: str = "UPDATE Hospital SET nome = %(name)s, endereco = %(address)s, cep = %(cep)s WHERE nome = %(hospital_name)s"
+        query_params: dict = {"name": name, "address": address, "cep": cep, "hospital_name": hospital_name}
+        cursor.execute(query, query_params)
+        db_conn.commit()
+    except:
+        print("Ocorreu um erro ao executar a acao")
+    finally:
+        print("Dados alter com sucesso!")
+        
+    cursor.close()
+    
+def delete_hospital(db_conn, name: str):
+    cursor = db_conn.cursor()
+
+    try:
+        query = "DELETE FROM Hospital WHERE nome = %(name)s"
+        query_params = {'name': name}
+        cursor.execute(query, query_params) # type: ignore
+        db_conn.commit()
+    except:
+        print("Falha ao deletar os dados")
+    finally:
+        affected_rows = cursor.rowcount
+        print(f"{affected_rows} registros atualizados.")
+
+
+        
